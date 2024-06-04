74728e79c314a20ae3e287fc769ace070265f5c8
LIinz 27 <105841100321@student.unismuh.ac.id>
2024-06-04 15:27:28 +0800

    Adding Animation Male -Not Finish

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,57 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+    private Animator animator;
 
-    private bool isMoving;
-
-    private Vector2 input;
-
-    public LayerMask solidObjectLayer;
-
-    private void Update()
+    void Start()
     {
-        if (!isMoving)
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * moveSpeed;
+
+        if (moveInput.x > 0) // Jika bergerak ke kanan
         {
-            input.x = Input.GetAxisRaw("Horizontal");
-            input.y = Input.GetAxisRaw("Vertical");
-
-            if (input.x != 0) input.y = 0;
-
-            if (input != Vector2.zero)
-            {
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
-
-                if(isWalkable(targetPos))
-                    StartCoroutine(Move(targetPos));
-            }
+            animator.SetBool("isWalkingRight", true);
+            animator.SetBool("isWalkingLeft", false);
+            animator.SetBool("isWalkingDown", false);
+            animator.SetBool("isWalkingUp", false);
+        }
+        else if (moveInput.x < 0) // Jika bergerak ke kiri
+        {
+            animator.SetBool("isWalkingRight", false);
+            animator.SetBool("isWalkingLeft", true);
+            animator.SetBool("isWalkingDown", false);
+            animator.SetBool("isWalkingUp", false);
+        }
+        else if (moveInput.y < 0) // Jika bergerak ke bawah
+        {
+            animator.SetBool("isWalkingRight", false);
+            animator.SetBool("isWalkingLeft", false);
+            animator.SetBool("isWalkingDown", true);
+            animator.SetBool("isWalkingUp", false);
+        }
+        else if (moveInput.y > 0) // Jika bergerak ke atas
+        {
+            animator.SetBool("isWalkingRight", false);
+            animator.SetBool("isWalkingLeft", false);
+            animator.SetBool("isWalkingDown", false);
+            animator.SetBool("isWalkingUp", true);
+        }
+        else
+        {
+            animator.SetBool("isWalkingRight", false);
+            animator.SetBool("isWalkingLeft", false);
+            animator.SetBool("isWalkingDown", false);
+            animator.SetBool("isWalkingUp", false);
         }
     }
 
-    IEnumerator Move(Vector3 targetPos)
+    void FixedUpdate()
     {
-        isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = targetPos;
-
-        isMoving = false;
-    }
-
-    private bool isWalkable(Vector3 targetPos)
-    {
-        if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectLayer) != null)
-        {
-            return false;
-        }
-        return true;
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
